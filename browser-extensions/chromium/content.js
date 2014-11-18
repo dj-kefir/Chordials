f62b7b38f05143393ea71019075159e9b054e0af
AkatovDA <akatovda@gmail.com>
2014-11-19 00:51:12 +0300

    js extension profile

--- browser-extensions/chromium/content.js
+++ browser-extensions/chromium/content.js
@@ -1,1 +1,17 @@
-console.log(chrome.i18n.getMessage("extName") + ' has been successfully loaded ; )');
+console.profile('Chordials CPU profiler');
+
+var extName = chrome.i18n.getMessage("extName");
+
+console.groupCollapsed(extName + ' initialization');
+console.time('Total time spent');
+
+/*
+* TODO Connect to VK API
+* TODO Get audios
+* TODO ChAPI queries
+ */
+
+console.timeEnd('Total time spent');
+console.groupEnd();
+
+console.profileEnd('Chordials CPU profiler');
